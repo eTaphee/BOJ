2996ee189f1ea4c73c3603c860c084819e66a324
thlee <e-tap@naver.com>
2023-09-07 16:43:03 +0900

    Create BOJ_2164.java

---
+++ src/BOJ_2164.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BOJ_2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while (true) {
+            if (queue.size() > 1) {
+                queue.poll();
+            }
+            if (queue.size() == 1) {
+                break;
+            }
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.poll());
+    }
+}
